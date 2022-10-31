3d299f1f3567e0722e431631eec949a72d31038d
AadityaBhoota <aadityabhoota@gmail.com>
2022-10-31 00:38:41 -0400

    starter code

--- app/src/main/java/com/example/cs348project/MainActivity.java
+++ app/src/main/java/com/example/cs348project/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.cs348project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean x = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void helloWorld(View v){
+        TextView t = (TextView) findViewById(R.id.textview);
+        if (x)
+            t.setText("Hi");
+        else
+            t.setText("Bye");
+        x = !x;
     }
 }
